e09ecb5e7cf30a1f09a5361776ea3a20e48494fe
Martin Slabber <martin.slabber@gmail.com>
2020-02-04 20:24:10 +0200

    Move config to ~/.config

--- README.rst
+++ README.rst
@@ -8,13 +8,22 @@ Setup
 Install strongswan and xl2tpd.
 Create the files username, password, and hostname in ~/.config/meraki-vpn/default
 these files should contain your username, password and the Meraki hostname that you connect to.
+Create a file routes in that directory that contain the CIDRs that are routed via the VPN. One per line.
+
 ::
         apt-get install -y strongswan xl2tpd
         mkdir -p ~/.config/meraki-vpn/default
         echo "martinslabber" > ~/.config/meraki-vpn/default/username
         echo "s3cr3t" > ~/.config/meraki-vpn/default/password
-        echo "sansa-gwgqgktkzz.dynamic-m.com
-        echo "example-wapqikrk2z.dynamic-m.com" > ~/.config/meraki-vp
+        echo "example-wapqikrk2z.dynamic-m.com" > ~/.config/meraki-vpn/default/hostname
+        echo "192.168.34.0/24" > ~/.config/meraki-vpn/default/routes
+
+The shared connection secret is also stored in the directory ~/.config/meraki-vpn/default.
+This secret is stored in the file named ipsec.secret and should have the following format.
+Replace AAAA with the shared secret. Keep the "'s.
+
+::
+        : PSK "AAAA"
 
 Based on
 https://gist.github.com/psanford/42c550a1a6ad3cb70b13e4aaa94ddb1c
--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 # Target VPN server, username, and password are kept in files.
-export VPNHOST=$(cat config/host)
-export USERNAME=$(cat config/user)
-export PASSWORD=$(cat config/password)
+export VPNHOST=$(cat ~/.config/meraki-vpn/default/hostname)
+export USERNAME=$(cat ~/.config/meraki-vpn/default/username)
+export PASSWORD=$(cat ~/.config/meraki-vpn/default/password)
 
 mkdir -p /tmp/ipsec
 envsubst < ipsec.conf > /tmp/ipsec/ipsec.conf
 envsubst < xl2tpd.conf > /tmp/ipsec/xl2tpd.conf
 envsubst <  options.l2tpd.client > /tmp/ipsec/options.l2tpd.client
 
-sudo cp config/ipsec.secrets /etc/ipsec.secrets
+sudo cp ~/.config/meraki-vpn/default/ipsec.secrets /etc/ipsec.secrets
 sudo cp /tmp/ipsec/ipsec.conf /etc/ipsec.conf
 sudo cp /tmp/ipsec/xl2tpd.conf /etc/xl2tpd/xl2tpd.conf
 sudo cp /tmp/ipsec/options.l2tpd.client /etc/ppp/options.l2tpd.client
@@ -25,7 +25,7 @@ sleep 2 # Wait for the device to be created.
 for n in $(seq 4); do ifconfig ppp0; res=$?; if [ $res -ne 0 ]; then  sleep 1; else break; fi; done
 sleep 4
 
-for ROUTE in $(cat config/route | grep -v "^#")
+for ROUTE in $(cat ~/.config/meraki-vpn/default/routes | grep -v "^#")
     do
     sudo ip route add $ROUTE dev ppp0
     done
